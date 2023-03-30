c2fd5b71a98a02de1bed6da1f5925d615d6efb34
redmac135 <ethanzhao1981@gmail.com>
2023-03-30 10:29:40 -0400

    move navbar to component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,8 @@
 <script lang="ts">
-	import ThemeToggle from './components/ThemeToggleButton.svelte';
-	import { darkMode } from './stores/stores';
+	import Navbar from './components/Navbar.svelte';
 
 	import WelcomePage from './pages/WelcomePage.svelte';
 	import ProjectCards from './components/ProjectCards.svelte';
-
-	// color mode control
-	let menu = 1;
-
-	$: logo = $darkMode === false ? '/images/blacklogo.png' : '/images/whitelogo.png';
 
 	// project cards
 	const projects = [
@@ -30,20 +24,7 @@
 	];
 </script>
 
-<header class="flex flex-col sm:flex-row items-center m-3">
-	<a href="/">
-		<img src={logo} class="mr-3 h-16 min-w-max" alt="Personal Logo" />
-	</a>
-	<div class="hidden sm:block text-3xl">Ethan Zhao</div>
-	<nav class="flex justify-center sm:ml-auto">
-		<a href="/" on:click|preventDefault={() => (menu = 1)} class="ml-6 text-2xl inline">Work</a>
-		<a href="/" on:click|preventDefault={() => (menu = 2)} class="ml-6 text-2xl inline">About</a>
-		<a href="/" class="ml-6 text-2xl inline">Resume</a>
-	</nav>
-	<div class="sm:ml-6 pt-2">
-		<ThemeToggle />
-	</div>
-</header>
+<Navbar />
 
 <WelcomePage />
 
---
+++ src/routes/components/Navbar.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import ThemeToggle from "./ThemeToggleButton.svelte";
+    import { darkMode } from "../stores/stores";
+
+    let menu = 1;
+
+    $: logo = $darkMode === false ? '/images/blacklogo.png' : '/images/whitelogo.png';
+</script>
+
+<header class="flex flex-col sm:flex-row items-center m-3">
+	<a href="/">
+		<img src={logo} class="mr-3 h-16 min-w-max" alt="Personal Logo" />
+	</a>
+	<div class="hidden sm:block text-3xl">Ethan Zhao</div>
+	<nav class="flex justify-center sm:ml-auto">
+		<a href="/" on:click|preventDefault={() => (menu = 1)} class="ml-6 text-2xl inline">Work</a>
+		<a href="/" on:click|preventDefault={() => (menu = 2)} class="ml-6 text-2xl inline">About</a>
+		<a href="/" class="ml-6 text-2xl inline">Resume</a>
+	</nav>
+	<div class="sm:ml-6 pt-2">
+		<ThemeToggle />
+	</div>
+</header>
