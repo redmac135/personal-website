ddd20a563e3375ea33b6dc5f707f07c8dcb2764f
redmac135 <ethanzhao1981@gmail.com>
2023-03-30 11:09:09 -0400

    remove tailwind from theme toggle

--- src/routes/components/ThemeToggleButton.svelte
+++ src/routes/components/ThemeToggleButton.svelte
@@ -8,7 +8,7 @@
 	}
 </script>
 
-<label class="relative inline-flex items-center cursor-pointer">
+<label class="wrapper">
 	<input
 		checked={$darkMode}
 		on:click={toggleTheme}
@@ -18,11 +18,50 @@
 	/>
 	<label
 		for="theme-toggle"
-		class="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 dark:after:translate-x-full dark:after:bg-transparent after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-amber-400 after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600"
+		class="button"
 	/>
 </label>
 
 <style lang="postcss">
+	.wrapper {
+		position: relative;
+		display: inline-flex;
+		align-items: center;
+		cursor: pointer;
+	}
+
+	.button {
+		width: 2.75rem;
+		height: 1.5rem;
+		border-radius: 999px;
+
+		background-color: #e5e7eb;
+	}
+
+	.button::after {
+		content: "";
+		position: absolute;
+		top: 2px;
+		left: 2px;
+		height: 1.25rem;
+		width: 1.25rem;
+		background-color: #fbbf24;
+		border-radius: 999px;
+		transition-property: all;
+    	transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+    	transition-duration: 150ms;
+	}
+
+	input:checked+.button {
+		background-color: #374151;
+		border-color: #4b5563;
+	}
+
+	input:checked+.button::after {
+		transform: translateX(100%);
+		background: transparent;
+	}
+
 	#theme-toggle {
 		display: none;
 	}
