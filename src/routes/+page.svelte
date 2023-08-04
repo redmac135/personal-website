74cdf66c66ff9e32ccadf15d3a7ad4c1d0ea081f
redmac135 <ethanzhao1981@gmail.com>
2023-08-04 13:02:33 -0400

    finish into animation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,25 @@
 <script lang="ts">
-	import Navbar from './components/Navbar.svelte';
+	let y = 0;
+	let innerHeight: number;
 
-	import WelcomePage from './pages/WelcomePage.svelte';
-	import ProjectCards from './components/ProjectCards.svelte';
+	function interpolate(
+		y: number,
+		startY: number,
+		endY: number,
+		startValue: number,
+		endValue: number
+	) {
+		const diffY = endY - startY;
+		const diffValue = endValue - startValue;
+		if (y < startY) {
+			return startValue;
+		} else if (startY <= y && y <= endY) {
+			const progress = (y - startY) / diffY;
+			return startValue + diffValue * progress;
+		} else if (endY < y) {
+			return endValue;
+		}
+	}
 
 	// project cards
 	const projects = [
@@ -24,4 +41,71 @@
 	];
 </script>
 
-<WelcomePage />
+<svelte:window bind:scrollY={y} bind:innerHeight />
+
+<!-- TODO: make animation more 3d like -->
+<!-- TODO: make these things disappear when not in view -->
+<main>
+	<div id="home">
+		<div id="darkener" style:opacity={interpolate(y, 0.7 * innerHeight, 1.8 * innerHeight, 0, 1)}></div>
+		<div
+			id="foreground-image"
+			style:transform="scale3d({interpolate(y, 0, 1 * innerHeight, 1, 2)},
+			{interpolate(y, 0, 1 * innerHeight, 1, 2)}, 1)"
+		/>
+		<div
+			id="background-image"
+			style:transform="scale3d({interpolate(y, 0, 1 * innerHeight, 1, 1.5)},
+			{interpolate(y, 0, 1 * innerHeight, 1, 1.5)}, 1)"
+		/>
+	</div>
+	<div id="about">THIS IS AN ABOUT ME</div>
+</main>
+
+<style>
+	main {
+		height: 500vh;
+		background-color: black;
+	}
+
+	#home {
+		height: 260vh;
+	}
+
+	#darkener {
+		position: fixed;
+		z-index: 2;
+		width: 100%;
+		height: 100vh;
+		background-color: black;
+		background-size: cover;
+	}
+
+	#foreground-image {
+		position: fixed;
+		width: 100%;
+		height: 100vh;
+		background-image: url('/images/room.png');
+		background-position: 50% 50%;
+		background-size: cover;
+		background-repeat: no-repeat;
+		z-index: 1;
+	}
+
+	#background-image {
+		position: fixed;
+		width: 100%;
+		height: 100vh;
+		background-image: url('/images/sunset.png');
+		background-position: 50% 50%;
+		background-size: cover;
+		background-repeat: no-repeat;
+		z-index: 0;
+	}
+
+	#about {
+		border-top: 5px white solid;
+		position: relative;
+		z-index: 3;
+	}
+</style>
