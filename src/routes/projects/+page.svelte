b2da16a63c25645c5b0bfa04d270005c5342b906
redmac135 <ethanzhao1981@gmail.com>
2023-09-19 15:36:14 -0400

    add basic meta tags to all pages

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -3,6 +3,12 @@
 	import Title from '$lib/components/Title.svelte';
 </script>
 
+<svelte:head>
+	<meta name="title" content="Projects" />
+	<meta name="description" content="A list of projects worked on and completed by Ethan Zhao" />
+	<title>Projects</title>
+</svelte:head>
+
 <main>
 	<Title>Projects</Title>
 	<ProjectCards />
