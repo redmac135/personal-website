8a7615c3cd9efefa9440eb5db3bbfed041ad7a0b
redmac135 <ethanzhao1981@gmail.com>
2023-11-15 00:34:39 -0500

    add footer framework

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { PROJECTS_LIST as projects } from '../../info';
+</script>
+
+<footer>
+	<div class="title">
+		<!-- TODO: add dyanmically changing logo -->
+		<div class="logo" />
+		<div class="text">Ethan Zhao</div>
+	</div>
+	<ul class="section">
+		<h2>Projects</h2>
+		{#each projects as project}
+			<a href={project.url}>{project.title}</a>
+		{/each}
+	</ul>
+	<ul class="section">
+		<h2>Socials</h2>
+		<!-- TODO: add socials here -->
+	</ul>
+</footer>
